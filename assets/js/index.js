7ef7095bd5f032385d6e9eea2e8c852708704ecc
Danial Alborz <danialalborz3@gmail.com>
2024-09-29 22:40:29 -0700

    create products
    and update index.html

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+let products = [
+    {
+        name: 'لپ تاپ 13.6 اینچی اپل مدل macBook Pro M3 2023 48gb Ram 1TB ssd',
+        img: './assets/img/macBook-Pro-M3-2023-48gbRam-1TBssd.webp',
+        color: 'خاکستری',
+        warranty: '12',
+        fastDelivery: 'true',
+        price: '200,000,000',
+        count: '1'
+    },
+    {
+        name: 'موبایل اپل مدل iphone 15 pro max 256gb',
+        img: './assets/img/apple-iphone-15.webp',
+        color: 'مشکی',
+        warranty: '24',
+        fastDelivery: 'false',
+        price: '180,000,000',
+        count: '2'
+    },
+]
