1a36300d70c37372546c8387f80a0c6bbfdd0f53
Danial Alborz <danialalborz3@gmail.com>
2024-09-29 20:35:42 -0700

    start and cr main
    create index.html
    add tailwind
    create main

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    fontFamily: {
+      'iran-sans': ['IranSans'],
+      'Mikhak': ['Mikhak']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
